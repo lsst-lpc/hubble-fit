31d275d3fce62a0ef32f56f56d10f51dd1484d0b
Sebastien Binet <binet@cern.ch>
2017-04-27 12:58:58 +0200

    hubble-fit: first Go skeleton

---
+++ main.go
@@ -0,0 +1,9 @@
+// Copyright 2017 The hubble-fit Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// hubble-fit is a program to fit the Hubble diagram with a Cosmological model.
+package main
+
+func main() {
+}
